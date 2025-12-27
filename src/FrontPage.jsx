18c232e858d3e6c687cf58f9375e6fda37985f50
silentwinter0 <silentwinter0@proton.me>
2025-12-28 00:22:15 +0200

    Imported about-styling from old portfolio

--- src/FrontPage.jsx
+++ src/FrontPage.jsx
@@ -1,16 +1,14 @@
 import './FrontPage.css'
 import Footer from './components/Footer.jsx'
 import Header from './components/Header.jsx'
-import MenuButton from './components/MenuButton'
+import About from './components/About.jsx'
 import ProjectContainer from './components/ProjectContainer'
 
 function FrontPage() {
 
   return (
     <div className="background">
-      <nav>
-        <Header/>
-      </nav>
+      <Header />
       <div className="card">
         <h1>Sami Manner</h1>
         <p>
@@ -27,20 +25,7 @@ function FrontPage() {
         </p>
         <ProjectContainer/>
       </div>
-      <div className="card">
-        <h1>Other work</h1>
-        <p>
-          Lorem ipsum etc.
-        </p>
-      </div>
-      <section id="about" className="about flex flex-col items-center justify-center pt-32 pb-32 text-center">
-        <h1 className="text-4xl font-semibold">ABOUT ME</h1>
-        <div className="selfportrait border-2 border-white bg-[#181A1E] w-3/5 md:w-1/3 mx-auto my-6 overflow-hidden">
-          <img src="./images/pic3.jpg" alt="A picture of the website author, myself." className="w-full h-auto object-cover" />
-        </div>
-        <p className="bigtext text-2xl font-medium">programmer/designer</p>
-        <p className="mt-2">something lorem ipsum.</p>
-      </section>
+      <About />
       <Footer/>
     </div>
   )
