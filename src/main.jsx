e8a82907d754b934abc675ec41d6dc149ab1636a
silentwinter0 <silentwinter@proton.me>
2025-12-27 13:35:51 +0200

    Renamed front page files

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import FrontPage from './FrontPage.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path ="/*" element={<App />} />
+        <Route path ="/*" element={<FrontPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
