12aa15e97f9c433a1b0375132611c125980726a9
silentwinter0 <silentwinter@proton.me>
2025-12-27 14:01:59 +0200

    Added placeholder About Me -section

--- src/FrontPage.jsx
+++ src/FrontPage.jsx
@@ -8,26 +8,23 @@ function FrontPage() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>FUTURE STUFF</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Lorem ipsum etc.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section id="about" className="about flex flex-col items-center justify-center pt-32 pb-32 text-center">
+        <h1 className="text-4xl font-semibold">ABOUT ME</h1>
+        <div className="selfportrait border-2 border-white bg-[#181A1E] w-3/5 md:w-1/3 mx-auto my-6 overflow-hidden">
+          <img src="./images/pic3.jpg" alt="A picture of the website author, myself." className="w-full h-auto object-cover" />
+        </div>
+        <p className="bigtext text-2xl font-medium">programmer/designer</p>
+        <p className="mt-2">something lorem ipsum.</p>
+      </section>
     </>
   )
 }
