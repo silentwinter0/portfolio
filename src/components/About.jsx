327770eee34f8ab1a3480299afbbfd77dbb8cad1
silentwinter0 <silentwinter@proton.me>
2025-12-28 00:22:15 +0200

    Imported about-styling from old portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import './About.css'
+
+export default function About() {
+  return (
+    <section id="about" className="about">
+      <h1>ABOUT ME</h1>
+      <div className="selfportrait">
+        <img src="../assets/images/pic1.jpg" alt="A picture of the website author, myself." />
+      </div>
+      <p className="bigtext">hey guess what it’s yet another programmer/designer person!</p>
+      <p>i’m very sure this person is a perfect fit for your company</p>
+    </section>
+  )
+}
