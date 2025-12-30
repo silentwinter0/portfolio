d8a93ed8af39ef0a996e30f5f5b5d12e101ce415
silentwinter0 <silentwinter@proton.me>
2025-12-30 19:30:55 +0200

    Adjusted project containers to have equal vertical height

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -8,10 +8,10 @@ export default function ProjectContainer() {
         <div className="top-projects">
           <ProjectCard
             imageSrc=""
-            title="Project Title"
-            subtitle="I broke the internet..."
-            tech="C++, my bare hands"
-            date="December 2019"
+            title="Societal Campaign Project"
+            subtitle="How can we raise awareness about misleading AI images? This project was praised by real professionals in the field of communications."
+            tech="Campaign design & strategy, research, web design (React/CSS)"
+            date="January 2025 – May 2025"
             link="./"
           />
           <ProjectCard
