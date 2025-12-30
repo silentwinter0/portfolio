c02a0f4202a877086f856e2cbee126cd3eca6370
silentwinter0 <silentwinter@proton.me>
2025-12-30 23:20:09 +0200

    Moved pages to appropriate folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import FrontPage from './FrontPage.jsx'
+import FrontPage from './pages/FrontPage.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
