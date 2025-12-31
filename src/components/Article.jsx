4679053b3fe5a63fb2081bf1191c81860321d81a
silentwinter0 <silentwinter0@proton.me>
2025-12-31 12:17:48 +0200

    Added article div for use in project windows

---
+++ src/components/Article.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Article.css'
+
+export default function Article({ imageSrc = '', title = '', subtitle = '', content = '', link = '#' }) {
+  return (
+    <article className="article">
+      <div className="article-projecttext">
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        <p>{content}</p>
+        <div className="article-readmore">
+          <a href={link}>Read more</a>
+        </div>
+      </div>
+    </article>
+  )
+}
