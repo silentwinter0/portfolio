ef0781630f70829d11314558ffe5e6307c5c5781
silentwinter0 <silentwinter@proton.me>
2025-12-31 16:28:41 +0200

    Worked on comms campaign site content

--- src/pages/CommunicationCampaign.jsx
+++ src/pages/CommunicationCampaign.jsx
@@ -1,8 +1,7 @@
 import './FrontPage.css'
 import Footer from '../components/Footer.jsx'
 import Header from '../components/Header.jsx'
-import About from '../components/About.jsx'
-import ProjectContainer from '../components/Projects.jsx'
+import Article from '../components/Article.jsx'
 
 function CommunicationCampaign() {
 
@@ -11,14 +10,25 @@ function CommunicationCampaign() {
       <Header />
       <div className="card">
         <h1>Communication Campaign – Teaching to recognize AI images</h1>
+        <p>January 2025 – May 2025</p>
         <p className='bigtext'>
-          How can we raise awareness about misleading AI images? <br/>
+          <br/>
+          How can we raise awareness about misleading AI images? <br/><br/>
+
           This campaign strategy and proof-of-concept game was praised by real 
           professionals in the field of communications for its clear strategy, 
-          noting it could be put to use as-is. <br/>
-          Campaign design & strategy, research, content creation. <br/>
-          Responsible for POC design and implementation (React/CSS)
+          noting it could be put to use as-is. <br/><br/>
+
+          Worked on campaign design & strategy, research, content creation. <br/>
+          Responsible for proof-of-concept design and implementation (React/CSS)
         </p>
+      </div>
+      <div className='card'>
+        <Article
+          title='Tunnistatko tekoälykuvat? – Would you recognize an AI-image?'
+          subtitle=''
+          content="Lorem ipsum"
+        />
       </div>
       <Footer />
     </div>
