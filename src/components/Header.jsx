5219f62e261c6f5758bbf7eb617b4073f77e9d83
silentwinter0 <silentwinter0@proton.me>
2026-01-01 17:23:18 +0200

    Added more sections to nav header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,9 @@ export default function Header() {
             <div className="navlinkbar">
               <a className="navlinkelement" href="../">Home</a>
               <a className="navlinkelement" href="../#projects">Projects</a>
-              <a className="navlinkelement" href="../#about">About Me</a>
+              <a className="navlinkelement" href="../#studies">Studies</a>
+              <a className="navlinkelement" href="../#other-work">Other work</a>
+              <a className="navlinkelement" href="../#about">About me</a>
             </div>
           </div>
         </div>
