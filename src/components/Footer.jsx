5324114a11dc4d8dd7d499ab8b1f326a0fef3502
silentwinter0 <silentwinter@proton.me>
2026-01-01 23:52:56 +0200

    Adjusted footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,19 +5,17 @@ export default function Footer() {
     <footer>
       <div className="footertext">
         <div className="footerdiv">
-          <h4 className="footer-header">more links</h4>
+          <h4 className="footer-header">links</h4>
           <p className="footer-text">lorem ipsum etc.</p>
         </div>
         <div className="footerdiv">
-          <h4 className="footer-header">contact info</h4>
-          <p className="footer">lorem.ipsum@example.com</p>
-          <p className="footer">+555 000 000 000</p>
-          <p className="footer">linkedin: blah blah</p>
+          <h4 className="footer-header">contact</h4>
+          <p className="footer">silentwinter0@proton.me</p>
         </div>
         <div className="footerdiv">
-          <h4 className="footer-header">legal info</h4>
-          <p className="footer-legal">"pic1.jpg" by Dietmar Rabich, CC BY-SA 4.0</p>
-          <p className="footer-legal">"pic2.jpg" by Basile Morin - Own work, CC BY-SA 4.0</p>
+          <h4 className="footer-header">legal</h4>
+          <p className="footer-legal">All trademarks 
+            referenced herein are the properties of their respective owners.</p>
         </div>
       </div>
     </footer>
