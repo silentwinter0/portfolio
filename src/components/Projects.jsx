c7d6eb8805b75fdb8f9c97752ca724708b2903b4
silentwinter0 <silentwinter@proton.me>
2026-01-01 23:22:33 +0200

    Adjusted project text

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,14 +8,18 @@ export default function Projects() {
         <div className="top-projects">
           <ProjectCard
             imageSrc=""
-            title="Communication Campaign – Teaching digital novices to 
+            title="Teaching digital novices to 
             recognize AI images"
-            subtitle="How can we raise awareness about misleading AI images? 
-            This campaign strategy and proof-of-concept game was praised by real
-            professionals in the field of communications for its 
-            clear strategy, noting it could be put to use as-is."
-            tech="Worked on campaign design & strategy formulation, research, 
-            content creation. Responsible for proof-of-concept design and 
+            subtitle={<>
+              How can we raise awareness about misleading AI images? 
+              This communication campaign strategy and proof-of-concept game was
+              praised by real professionals in the field of communications for 
+              its clear strategy, noting it could be put to use as-is. 
+              <br/><br/>
+              Worked on campaign design & strategy formulation, research, 
+              and content creation.
+            </>}
+            tech="Responsible for proof-of-concept design and 
             implementation with React."
             date="January 2025 – May 2025"
             link="./projects/communication-campaign"
@@ -23,9 +27,12 @@ export default function Projects() {
           <ProjectCard
             imageSrc=""
             title="Command-line Scheduler in C"
-            subtitle="Simple scheduling system for scheduling one-hour meeting 
-            times. Written from scratch in C as a console application, featuring
-             file I/O to save and load schedules."
+            subtitle={<>
+              Simple scheduling system for scheduling one-hour meeting 
+              times. <br/><br/>
+              Written from scratch in C as a console application under a strict 
+              timeframe, featuring file I/O to save and load schedules.
+            </>}
             tech="Written in C"
             date="March 2025"
             link="./projects/c-scheduler"
@@ -33,10 +40,12 @@ export default function Projects() {
           <ProjectCard
             imageSrc=""
             title="This portfolio site!"
-            subtitle="This site was built by me using React and CSS. Initially 
-            starting out as a simple HTML/CSS-site completed as coursework, I 
-            have since reworked the website from the ground to better showcase 
-            my current skills and expertise."
+            subtitle={<>
+              This site was built by me using React and CSS. Initially 
+              starting out as a simple HTML/CSS-site completed as coursework, I 
+              have since reworked the website from the ground to better showcase 
+              my current skills and expertise.
+            </>}
             tech="Developed with React, CSS"
             date="December 2025"
             link="./"
