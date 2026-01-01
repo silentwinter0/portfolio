8a854def4c471b4cb213fef48548ec58b0a91682
silentwinter0 <silentwinter@proton.me>
2026-01-02 00:17:49 +0200

    Added final section to nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,12 @@ export default function Header() {
       <div className="linkbarholderholder">
         <div className="linkbarholder">
           <div className="topofnav">
-            <MenuButton/>
             <div className="navlinkbar">
               <a className="navlinkelement" href="../">Home</a>
               <a className="navlinkelement" href="../#projects">Projects</a>
               <a className="navlinkelement" href="../#studies">Studies</a>
               <a className="navlinkelement" href="../#other-work">Other work</a>
+              <a className="navlinkelement" href="../#work-experience">Work experience</a>
               <a className="navlinkelement" href="../#about">About me</a>
             </div>
           </div>
