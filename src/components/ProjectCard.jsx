bd0855dd4e98bf2fb78d92f14a67904f0a91ce3e
silentwinter0 <silentwinter0@proton.me>
2026-01-01 22:33:18 +0200

    Bottom-aligned date and tech fields for projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -9,8 +9,10 @@ export default function ProjectCard({ imageSrc, title, subtitle, tech, date, lin
       <div className="projecttext">
         <h2>{title}</h2>
         <h3>{subtitle}</h3>
-        <p>{tech}</p>
-        <p>{date}</p>
+        <div className="tech-date">
+          <p>{tech}</p>
+          <p>{date}</p>
+        </div>
         <a href={link}>Read more {'-->'}</a>
       </div>
     </div>
