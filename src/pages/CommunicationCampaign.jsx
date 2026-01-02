ee90449ddc54f343c5bfbac2b81eb0ee433053ec
silentwinter0 <silentwinter0@proton.me>
2026-01-02 14:22:21 +0200

    Wrote first version of comms campaign text

--- src/pages/CommunicationCampaign.jsx
+++ src/pages/CommunicationCampaign.jsx
@@ -10,7 +10,7 @@ function CommunicationCampaign() {
       <Header />
       <div className="card">
         <h1>Teaching digital novices to 
-            recognize AI images – A communication campaign</h1>
+            recognize AI images and disinformation – A communication campaign</h1>
         <p>January 2025 – May 2025</p>
         <p className='bigtext'>
           <br/>
@@ -29,9 +29,102 @@ function CommunicationCampaign() {
       <div className='card'>
         <Article
           title='Would you recognize an AI-image?'
-          subtitle=''
+          subtitle='THE PLAN'
           content={<>
-            Lorem ipsum
+            This group project was completed for a course in communication and 
+            digital media. Our task was to design a communication campaign, 
+            seeking to inform a particular audience or to cause a change in the
+            social climate, and to select a company that could perform this 
+            campaign. We chose to focus on informing those less technically 
+            inclined of the prevalence of AI images and mis- and disinformation 
+            making use of them on social media. 
+            <br/><br/>
+            Our campaign was one of three selected by course organizers to be 
+            presented to Vapa Media, a marketing and communications company, 
+            whose employees praised its clear strategy, idea and execution, 
+            stating it could be put to use as-is. In fact, they offered to reach
+            out in case 
+            <br/><br/>
+            Our selected course audience was the "older generation" of Finnish 
+            social media users: users of Facebook, aged 50 and above, and are 
+            less likely to understand the newest technologies and particularly 
+            how much image generation models have advances in recent years. This 
+            poses a significant issue, as these models can be and have already 
+            been used to generate intentionally misleading images for propaganda
+            and influencing public opinion, by both private and state actors. 
+            Additionally, our target audience values their family and stability 
+            of life most of all.
+            <br/><br/>
+            In summary, our strategy was as follows: Our aim is for older 
+            internet users to recognize the disinformation created by AI models 
+            and to understand its impact on their own lives and social media 
+            feeds. Our plan was to measure success with the number of visitors 
+            to the campaign website, and with the help of the AI image 
+            recognition game presented on the website, where our measure of 
+            success is the improvement of results between two rounds of the game.
+            <br/><br/>
+            The major challenge is that target group's understanding of the 
+            subject of AI is weak, and they do not know how to take the potential
+            disinformation threat seriously. Furthermore, AI creates a lot of 
+            confusion and fear, which helps our campaign as people are paying 
+            attention and keen to learn more about it. Thus, we need to 
+            communicate to them in a way that is both attention-grabbing and 
+            believable. We use examples to tell them how disinformation 
+            created with the help of artificial intelligence can have a negative
+            impact on their lives, and on the other hand we teach them to 
+            recognize the typical characteristics of AI images.
+            <br/><br/>
+            We elected to use the REAN (Reach, Engage, Activate, Nurture) model 
+            for our campaign strategy. Our core elements were the campaign 
+            website, which hosts a game challenging users to recognize AI images,
+            along with news articles and an ongoing campaign about AI images to
+            be published on the social media and web magazines of Ilta-Sanomat, 
+            which we chose as the ficticious backers of this campaign. They were
+            chosen due to having the right blend of believability, engaging
+            sensationalism, and target audience reach, making them uniquely 
+            suited to run the campaign.
+            <br/><br/>
+            The initial stage was reaching our audience by high-visibility 
+            campaigning, such as front-page articles relating to the topic, 
+            outdoor advertising, and social media challenges, inviting readers
+            to challenge themselves with the game. Then, the target group is 
+            engaged to open the website, where they will be activated to play 
+            the game, read more about the topic, and to share their results with
+            friends on social media, further nurturing the campaign. Another 
+            core idea was that once the website was up and running, the campaign
+            would be easy and efficient to run, as the target group would do most
+            of the work further spreading the campaign, while the campaigners 
+            could focus on releasing topical articles about recent developments 
+            in AI image generation. 
+          </>}
+        />
+        <Article
+          title='The proof-of-concept game'
+          subtitle='THE EXECUTION'
+          content={<>
+            After the planning stage was completed, we got to work on the 
+            execution. I was in charge of implementing most of the AI image 
+            recognition game, while my group members were in charge of selecting 
+            fonts, creating UI elements and the additional parts of the campaign,
+            such as the bus stop ads, article mockups and social media posts. 
+            We considered several different approaches for the game before 
+            landing on a very simple, swiping-based game that could work well on
+            both computers and phones. Drawing inspiration from the UI of dating
+            apps, the player is presented with an image, and they are prompted 
+            to swipe AI images to the left, and real images to the right.
+            <br/><br/>
+            After the first round, the user is presented with an overview of the
+            AI images presented, and how the user could recognize them going 
+            forward. Then, the second round begins, and after the game ends, 
+            they are presented with their score, along with encouragement to 
+            share their results and to keep practicing. The other pages of the
+            website also provide additional resources to learn about AI, such as
+            a general overview of what AI images are, as well as recent relevant
+            and curated articles.
+            <br/><br/>
+            You can play the game and see our campaign website <a 
+            href='https://campaign-project-jade.vercel.app/'>here</a>. Note that
+            the website and game are in Finnish.
           </>}
         />
       </div>
