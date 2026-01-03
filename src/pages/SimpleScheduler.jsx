0c741adf69f0ae3765456a36d67a84a64e8ea047
silentwinter0 <silentwinter@proton.me>
2026-01-03 23:52:11 +0200

    Wrote content for the scheduler page

--- src/pages/SimpleScheduler.jsx
+++ src/pages/SimpleScheduler.jsx
@@ -14,28 +14,41 @@ function SimpleScheduler() {
     <div className="background">
       <Header />
       <div className="card">
-        <h1>TODO</h1>
+        <h1>Command-line Scheduler, written in C</h1>
         <p>March 2025</p>
         <p className='bigtext'>
           <br/>
-           TODO
+            Simple scheduling system for scheduling one-hour meeting 
+            times. <br/><br/>
+            Written from scratch in C as a console application under a strict 
+            timeframe, featuring file I/O to save and load schedules.
         </p>
       </div>
       <div className='card'>
         <Article
-          title='TODO'
-          subtitle='TODO'
+          title='The Command-line Scheduler'
+          subtitle=''
           content={<>
             <figure className='inline-article-figure'>
               <img
                 src={pic1}
-                alt='TODO'
+                alt='A picture of the command-line scheduler interface while in 
+                use, showing command and their effects.'
                 className='inline-article-image'
               />
-              <figcaption className='inline-article-caption'>TODO</figcaption>
+              <figcaption className='inline-article-caption'>The command-line scheduler in action</figcaption>
             </figure>
             <br/><br/>
-            TODO
+            This simple C program was written as a part of my course in the 
+            basics of C programming. It features simple commands for writing to 
+            (W) and loading from (O) files, listing (L) current meetings, as 
+            well as adding (A) or deleting (D) meetings with a specified time. 
+            This was my first experience with writing a full program entirely 
+            from  scratch, as well as the final course project showing my skills
+            writing code in C.
+            <br/><br/>
+            The program itself is quite simple, and I completed it in a few days,
+            well ahead of schedule.
           </>}
         />
       </div>
