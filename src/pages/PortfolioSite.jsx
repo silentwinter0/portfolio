e5f57c5198cab3a28f3ed0e8253330e2f231b566
silentwinter0 <silentwinter@proton.me>
2026-01-06 19:31:44 +0200

    Added portfolio metasite

---
+++ src/pages/PortfolioSite.jsx
@@ -0,0 +1,54 @@
+import './FrontPage.css'
+import Footer from '../components/Footer.jsx'
+import Header from '../components/Header.jsx'
+import Article from '../components/Article.jsx'
+import { useState } from 'react'
+import Hero from '../components/Hero.jsx'
+
+// Pics
+import pic1 from '../assets/images/projects/portfoliosite/figma.png'
+
+function PortfolioSite() {
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  return (
+    <div className="background">    
+      <Hero />
+      <Header />
+      <div className="card">
+        <h1>Portfolio site in React</h1>
+        <p>December 2025 – January 2026</p>
+        <p className='bigtext'>
+          <br/>
+            This site was built by me using React and CSS. Initially 
+            starting out as a simple HTML/CSS-site completed as coursework, I 
+            have since reworked the website from the ground to better showcase 
+            my current skills and expertise.
+        </p>
+      </div>
+      <div className='card'>
+        <Article
+          title='Coming soon!'
+          subtitle=''
+          content={<>
+            <figure className='inline-article-figure'>
+              <img
+              src={pic1}
+              alt='Social post mockup'
+              className='inline-article-image'
+              />
+              <figcaption className='inline-article-caption'>My original website
+                 design plan from January 2025, including a moodboard for design
+                 inspiration.</figcaption>
+            </figure>
+            <br/>
+            Lorem ipsum
+          </>}
+        />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default PortfolioSite
