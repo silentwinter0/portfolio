8291e97d17536cd92e83225a9ab8a326c7bc1b8b
silentwinter0 <silentwinter0@proton.me>
2026-01-06 19:01:35 +0200

    Added image to meta project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import './Projects.css'
 import ProjectCard from './ProjectCard'
 import pic1 from '../assets/images/projects/comms/game2.png'
 import pic2 from '../assets/images/projects/simplescheduler/scheduler.png'
-import pic3 from '../assets/images/projects/comms/game2.png'
+import pic3 from '../assets/images/projects/portfoliosite/figma.png'
 
 export default function Projects() {
   return (
