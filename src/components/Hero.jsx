a7e70e12aae85611f4795d4575cfa14d56eef2c2
silentwinter0 <silentwinter0@proton.me>
2026-01-06 18:18:40 +0200

    Added more waves, temporarily removed interference effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,26 +24,25 @@ const Hero = () => {
             time += 0.01;
             ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-            ctx.beginPath();
-            ctx.strokeStyle = 'white';
-            ctx.lineWidth = 1.5;
+            const waveCount = 40;
 
-            const centerY = canvas.height / 2;
-            const amplitude = 100;
-            const frequency = 0.01;
+            for (let i = 0; i < waveCount; i++) {
+                
+                ctx.beginPath();
+                ctx.lineWidth = 1.5;
+                ctx.strokeStyle = `rgba(25, 0, 155)`;
 
-            for (let x = 0; x <= canvas.width; x += 5) {
-                const wave1 = Math.sin(x * frequency + time) * amplitude;
-                const wave2 = Math.sin(x * frequency * 2.5 + time) * (amplitude/7 * 0.4);
-                const y = centerY + wave1 + wave2;
+                const yOffset = (canvas.height * 0.2) + (i * 15);
+                const amplitude = 50;
+                const frequency = 0.01;
 
-                if (x === 0) {
-                    ctx.moveTo(x, y);
-                } else {
-                    ctx.lineTo(x, y);
+                for (let x = 0; x <= canvas.width; x += 5) {
+                    const y = yOffset + Math.sin(x * frequency + time + (i * 0.5)) * amplitude;
+                    if (x === 0) ctx.moveTo(x, y);
+                    else ctx.lineTo(x, y);
                 }
+                ctx.stroke();
             }
-            ctx.stroke();
 
             animationFrameId = window.requestAnimationFrame(render);
         };
